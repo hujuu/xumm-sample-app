789fa7fd197e2daace755dc4b2cddb9362716d3b
hujuu <1961946+hujuu@users.noreply.github.com>
2024-06-10 07:25:10 +0900

    NFTのmint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Xumm } from 'xumm'
+import {convertStringToHex} from "xrpl";
 
 const xumm = new Xumm("a77f5963-be7d-4451-b7ff-7717adf7fe0f");
 
@@ -30,6 +31,22 @@ function App() {
         }
     };
 
+    const mintNFT = async () => {
+        const payload = await xumm.payload?.create({
+            TransactionType: "NFTokenMint",
+            Account: account,
+            TransferFee: 5 * 1000, // 5%
+            URI: convertStringToHex('ipfs://QmTu17csW1DrFY9xcgH9efBNvMgo39JZwK2adHLpmsJQFR'),
+            NFTokenTaxon: 0, // 0は一般的なトークン
+            Flags: 1 + 8, // Burnable, Transferable
+        });
+
+        if (!payload?.pushed) {
+            // Xummへプッシュ通知が届かない場合
+            // payload?.refs.qr_png を利用してQRコードを表示することで署名画面を表示することも可能
+        }
+    };
+
     return (
         <div>
             {account && (
@@ -37,6 +54,7 @@ function App() {
                     <div>{account}</div>
                     <button onClick={disconnect}>Disonnect</button>
                     <button onClick={createTransaction}>Payment</button>
+                    <button onClick={mintNFT}>Mint NFT</button>
                 </>
             )}
             {!account && <button onClick={connect}>Connect</button>}
