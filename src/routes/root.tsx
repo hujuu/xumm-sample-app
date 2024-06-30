b13628ceaa92fd24347d94df3c3cb5dfd4cd8092
hujuu <1961946+hujuu@users.noreply.github.com>
2024-06-30 18:06:44 +0900

    Update root.tsx

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from 'react';
-import {Client, AccountNFTsRequest, AccountNFToken, convertHexToString} from 'xrpl';
 import Header from "../components/Header";
 import ItemList from "../components/ItemList";
 import HeroImage from "../components/HeroImage";
 import { xumm } from "../store/XummStore";
+import ItemListMarket from "../components/ItemListMarket.tsx";
 
 export default function Root() {
     const [account, setAccount] = useState<string | undefined>(undefined);
-    const [nfts, setNfts] = useState<AccountNFToken[] >([]);
 
     useEffect(() => {
         xumm.user.account.then((account) => setAccount(account));
@@ -20,53 +19,14 @@ export default function Root() {
     const disconnect = async () => {
         await xumm.logout();
         setAccount(undefined);
-        setNfts([]);
-    };
-
-    const fetchNFTs = async () => {
-        if (!account) return;
-
-        const client = new Client('wss://testnet.xrpl-labs.com');
-        await client.connect();
-
-        const request: AccountNFTsRequest = {
-            command: 'account_nfts',
-            account: account,
-        };
-
-        const response = await client.request(request);
-        setNfts(response.result.account_nfts);
-
-        await client.disconnect();
     };
 
     return (
         <main>
             <Header account={account} onConnect={connect} disConnect={disconnect}/>
             <HeroImage account={account} onConnect={connect} />
+            <ItemListMarket account={"r9GXFFfbgcWi1d5iU5Z2UdzKRE3btfAq8G"} />
             <ItemList account={account} />
-            {account && (
-                <div className="max-w-3xl mx-auto">
-                    <button className="btn" onClick={fetchNFTs}>所有しているNFTを確認する</button>
-                    {nfts.length > 0 && (
-                        <div>
-                            <h3>所有している NFTs</h3>
-                            <ul>
-                                {nfts.map((nft: AccountNFToken, index) => (
-                                    <li key={index}>
-                                        {JSON.stringify(nft)}
-                                        {nft.URI && <img
-                                            alt={nft.NFTokenID}
-                                            src={`${convertHexToString(nft.URI).replace("ipfs://", "")}`}
-                                        />}
-                                        {nft.URI && <p>{convertHexToString(nft.URI)}</p>}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
-                </div>
-            )}
         </main>
     );
 }
